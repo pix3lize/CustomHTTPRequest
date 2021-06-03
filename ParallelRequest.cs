271228250377cba532356ceee1c6f2e3f2cc8b89
pokemonball <85287411+pokemonball@users.noreply.github.com>
2021-06-03 19:47:13 +0800

    Enhancement multithread http request Fixes #2

---
+++ ParallelRequest.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Net.Security;
+
+namespace CustomHTTPRequestNS
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ParallelRequest : CustomHTTPRequest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ParallelRequest()
+        {
+            System.Net.ServicePointManager.Expect100Continue = false;
+            UserAgent = "Agent X";
+
+            XDeveloper = "";
+            this.Timeout = 120 * 1000;
+            CharEncode = Encoding.Default;
+        }
+    }
+}
