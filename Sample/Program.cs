c629a5ef42e96e8bfa053016165fbafa8f0120a9
pix3lize <hendri.ciptowibowo@gmail.com>
2021-11-25 22:50:38 +0800

    Basic auth with cookies ref

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
 using CustomHTTPRequestNS;
 
 namespace Sample
@@ -7,26 +10,49 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             CustomHTTPRequest CRequest = new CustomHTTPRequest();
             CustomWebResponse CResponse = new CustomWebResponse();
 
-            CResponse = CRequest.HTTPCustomRequest("https://www.google.com");
+            CookieContainer CContainer = new CookieContainer();
 
-            //GET  https://www.google.com 200 OK
-            Console.WriteLine(CResponse.Method + "  " +
-                CResponse.UrlRequest + " " +
-                CResponse.StatusCode + " " +
-                CResponse.Status);
+            // CResponse = CRequest.HTTPCustomRequest("https://docushare.xerox.com/doug/dsweb/ApplyLogin", "username=hendri.tjiptowibowo@sgp.fujixerox.com&password=FX_oofsc123&domain=DocuShare&Login=Login", ref CContainer);
+            // CResponse.SafeToFile(@"C:\Development\DocushareDownload\theresponse.txt");
+            // //GET  https://www.google.com 200 OK
+            // Console.WriteLine(CResponse.Method + "  " +
+            //     CResponse.UrlRequest + " " +
+            //     CResponse.StatusCode + " " +
+            //     CResponse.Status);
 
-            //Time taken : 00:00:00:05
-            Console.WriteLine("Time taken : " + CResponse.TimeTaken);
+            // //Time taken : 00:00:00:05
+            // //Console.WriteLine("Time taken : " + CResponse.TimeTaken);
+            // //Console.WriteLine("Response : " + CResponse.Response);
 
-            //REST API Sample 
+            // CRequest.ReturnStream = true;
+            // CResponse = CRequest.HTTPCustomRequest(@"https://docushare.xerox.com/doug/dsweb/Get/Document-121470", CContainer);
+
+
+            // CResponse.SafeToFile(@"C:\Development\DocushareDownload\newfile.pdf");
+
+            // Console.WriteLine("Hello World!");
+            // CustomHTTPRequest CRequest = new CustomHTTPRequest();
+            // CustomWebResponse CResponse = new CustomWebResponse();
+
+            // CResponse = CRequest.HTTPCustomRequest("https://www.google.com");
+
+            // //GET  https://www.google.com 200 OK
+            // Console.WriteLine(CResponse.Method + "  " +
+            //     CResponse.UrlRequest + " " +
+            //     CResponse.StatusCode + " " +
+            //     CResponse.Status);
+
+            // //Time taken : 00:00:00:05
+            // Console.WriteLine("Time taken : " + CResponse.TimeTaken);
+
+            //REST API Sample
             RESTRequest RRequest = new RESTRequest();
             CResponse = new CustomWebResponse();
 
-            CResponse = RRequest.RESTGETBearer(@"https://api.sandbox.transferwise.tech/v1/profiles","");
+            CResponse = RRequest.RESTBearer(@"https://api.sandbox.transferwise.tech/v1/profiles", RESTRequest.Method.GET, "", "TokenHere");
 
             //GET  https://www.google.com 200 OK
             Console.WriteLine(CResponse.Method + "  " +
